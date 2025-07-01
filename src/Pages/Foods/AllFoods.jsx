8b5bd207aa5c28c474a704f969d7633b74d848df
Saif-Smran <23303079@iubat.edu>
2025-07-01 20:08:10 +0600

    Add 4 Cards

--- src/Pages/Foods/AllFoods.jsx
+++ src/Pages/Foods/AllFoods.jsx
@@ -157,7 +157,7 @@ const AllFoods = () => {
                 </div>
 
                 {/* Food Grid */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-8">
                     {filteredFoods.map(food => (
                         <FoodCard key={food._id} food={food} />
                     ))}
